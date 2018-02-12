fe9e8fa671462e53eb7938705bb6c8ef709fce03
PIOTR\Piotr <piotrt337@gmail.com>
2018-02-13 00:49:02 +0100

    Special for Tabisz

--- Survival Game/Assets/PeopleStuff/Piotr/Scripts/CharacterAnimatorPT.cs
+++ Survival Game/Assets/PeopleStuff/Piotr/Scripts/CharacterAnimatorPT.cs
@@ -8,12 +8,22 @@ public class CharacterAnimatorPT : MonoBehaviour {
 
 	void Start ()
     {
-        animator = GetComponentInChildren<Animator>(); //ustawienie animatora z dziecka Playera
+        animator = GetComponent<Animator>(); //ustawienie animatora
 	}
 	
 	
 	void Update ()
     {
-		
-	}
+        float LocomotionBlend = 0.5f;
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+
+        animator.SetFloat("LocomotionBlend", LocomotionBlend, .1f, Time.deltaTime);
+
+
+    }
 }
