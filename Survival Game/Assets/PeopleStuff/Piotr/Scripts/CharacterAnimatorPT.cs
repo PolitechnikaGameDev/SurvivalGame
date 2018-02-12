219adbeefb3ccf8757d497424d5d786a7fda5a8d
PIOTR\Piotr <piotrt337@gmail.com>
2018-02-13 00:15:51 +0100

    Added animator controller
    -Added animator controller
    -Player materials changed
    -Animator Script added (just started writing)

---
+++ Survival Game/Assets/PeopleStuff/Piotr/Scripts/CharacterAnimatorPT.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimatorPT : MonoBehaviour {
+
+    Animator animator; //dostep do animator controllera
+
+	void Start ()
+    {
+        animator = GetComponentInChildren<Animator>(); //ustawienie animatora z dziecka Playera
+	}
+	
+	
+	void Update ()
+    {
+		
+	}
+}
