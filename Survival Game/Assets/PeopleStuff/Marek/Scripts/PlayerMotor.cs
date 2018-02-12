cab2f05c1d940d4a1f00fa4878d3aec9794bece2
MarekTabiszewski <marek.tabiszewski97@gmail.com>
2018-02-13 00:41:38 +0100

    Bugfix + comments

--- Survival Game/Assets/PeopleStuff/Marek/Scripts/PlayerMotor.cs
+++ Survival Game/Assets/PeopleStuff/Marek/Scripts/PlayerMotor.cs
@@ -13,27 +13,31 @@ public class PlayerMotor : MonoBehaviour {
 	
 	void Update () {
         Vector3 input = HandleInput();
-        transform.Translate(input* speed * Time.deltaTime);
-        Quaternion rotation = Quaternion.LookRotation(input);
-        transform.GetChild(0).transform.rotation = rotation;
+        if (input.magnitude != 0)
+        {
+            transform.Translate(input * speed * Time.deltaTime);// ruch
+
+            Quaternion rotation = Quaternion.LookRotation(input);
+            transform.GetChild(0).transform.rotation = rotation;//rotacja samego modelu
+        }
 
 
     }
 
-    private Vector3 GetInput()
+    private Vector3 GetInput()// pobieranie inputu
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        Vector3 input = new Vector3(vertical,0, horizontal);
+        Vector3 input = new Vector3(vertical,0, horizontal);// zamiast 'zera' moze byc cos co bedzie wykorzystywane do skakania
         return input;
     }
 
 
-    private Vector3 HandleInput()
+    private Vector3 HandleInput()//przerabia input tak zeby dostac kierunek ruchu dla gracza
     {
         Vector3 input = GetInput();
         Vector3 forward = Camera.main.transform.forward;
-        Vector3 right = Camera.main.transform.right;
+        Vector3 right = Camera.main.transform.right;//kierunek na podstawie lokalnych kierunkow kamery
         forward.y = 0f;
         right.y = 0f;
         forward.Normalize();
