c9b19229b6e0410d05430c3134825d2ea2f2f142
PIOTR\Piotr <piotrt337@gmail.com>
2018-02-15 13:53:36 +0100

    Added terrain

--- Survival Game/Assets/PeopleStuff/Piotr/Scripts/CameraFollow.cs
+++ Survival Game/Assets/PeopleStuff/Piotr/Scripts/CameraFollow.cs
@@ -6,24 +6,13 @@ public class CameraFollow : MonoBehaviour {
 
     public float CameraMoveSpeed = 120.0f; //jak szybko porusza sie kamera
     public GameObject CameraFollowObj; //co sledzi nasza kamera
-    //Vector3 FollowPOS;
     public float clampAngleH = 66.0f; //o jaki kat moze sie obrocic kamera w poziomie(glowa)
     public float clampAngleL = -40.0f; //o jaki kat moze sie obrocic kamera w poziomie(nogi)
     public float inputSensitivity = 150.0f; //jak czula jest kamera
-    //public GameObject CameraObj;
-    //public GameObject PlayerObj;
-    //public float camDistanceXToPlayer;
-    //public float camDistanceYToPlayer;
-    //public float camDistanceZToPlayer;
     public float mouseX;
     public float mouseY;
-    //public float smoothX;
-    //public float smoothY;
     private float rotY = 0.0f;
     private float rotX = 0.0f;
-
-
-
 
     void Start ()
     {
@@ -31,7 +20,7 @@ public class CameraFollow : MonoBehaviour {
         rotY = rot.y;
         rotX = rot.x;
         //Cursor.lockState = CursorLockMode.Locked;
-        //Cursor.visible = false;
+        Cursor.visible = false;
 	}
 	
 	
